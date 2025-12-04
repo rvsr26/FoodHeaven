e9526cb287e9fb2b945eaad76a197baa09120c04
rvsr26 <145008299+rvsr26@users.noreply.github.com>
2025-12-04 10:15:00 +0530

    feat: add signup / signin validation functions

--- js/signin.js
+++ js/signin.js
@@ -1,2 +1,3 @@
 import { auth } from './firebase_config.js';
 console.log("Authentication Controller Initialized");
+export function validateEmail(email) { return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email); }
