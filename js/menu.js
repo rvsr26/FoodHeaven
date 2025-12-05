2858b32858fb5b1f47593a5ccf354a7fabaab07a
rvsr26 <145008299+rvsr26@users.noreply.github.com>
2025-12-05 14:15:00 +0530

    refactor: extract cart state methods into helper hooks

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,4 @@
 /* Food Heaven Menu & Cart State */
 export let cart = JSON.parse(localStorage.getItem('cart')) || [];
 export function saveCart() { localStorage.setItem('cart', JSON.stringify(cart)); }
+// Helper methods
