25c309cf58f20b0cf049537cb9692921809ba037
rvsr26 <145008299+rvsr26@users.noreply.github.com>
2025-12-09 14:15:00 +0530

    refactor: optimize data payloads sent to firestore client

--- js/admin.js
+++ js/admin.js
@@ -2,3 +2,4 @@ console.log("Admin Dashboard scripts loaded");
 export function addMenuItem(item) { console.log("Added item", item); }
 export function deleteMenuItem(id) { console.log("Deleted item", id); }
 // CRUD comments added
+// Payloads compression hook
