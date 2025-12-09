58b3920c53c11e3a0c672cd4f23028b3b386ea07
rvsr26 <145008299+rvsr26@users.noreply.github.com>
2025-12-09 11:30:00 +0530

    docs: add detailed comments for database query structures

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,4 @@
 console.log("Admin Dashboard scripts loaded");
 export function addMenuItem(item) { console.log("Added item", item); }
 export function deleteMenuItem(id) { console.log("Deleted item", id); }
+// CRUD comments added
